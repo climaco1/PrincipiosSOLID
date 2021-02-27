0d96095f9b07aa7109c4d7a99af6c631b10762a2
cristiancast <74682758+cristiancast@users.noreply.github.com>
2021-02-27 12:27:43 -0500

    Implemente los puntos 2, 3, 4 del taller

--- src/main/java/co/unicauca/parkinglot/domain/TruckParkingCost.java
+++ src/main/java/co/unicauca/parkinglot/domain/TruckParkingCost.java
@@ -12,18 +12,33 @@ import java.time.LocalDateTime;
  * @author fermt
  */
 public class TruckParkingCost implements IParkingCost {
+    private long result = 0;
+    private boolean ganoSorteo = false;
+    
     @Override
     public long calculateCost(Vehicle veh, LocalDateTime input, LocalDateTime output){
-        long result = 0;
-        
-        int totalHoras = output.getHour() - input.getHour();
-        int minutosTranscurridos = output.getMinute() - input.getMinute();
-        
-        if(totalHoras <= 1 && minutosTranscurridos <= 0){
-            result = 2000;
+        if(ganoSorteo){
+           return 0;
         }else{
-            result = 2000 + (((totalHoras-1)*60 + minutosTranscurridos) * 1000)/60;
+             int totalDias = output.getDayOfMonth() - input.getDayOfMonth();
+            int totalHoras = output.getHour() - input.getHour();
+
+            if(totalHoras <= 12 && totalDias == 0){
+                result = 10000;
+            }else{
+                if( totalHoras > 12 && totalHoras <=24 && totalDias == 0){
+                    result = 15000;
+                }else{
+                    result = 15000 + (((totalDias-1)*24 + totalHoras) * 15000)/24;
+                }    
+            }
+            return result;
         }
-        return result;
+    }
+    
+    public boolean sortearCon(int prmNumero){
+        int numeroAleatorio = (int)(Math.random() * 1000) + 1; //Encuantra un numero aleatorio entre 1-1000
+        ganoSorteo = (numeroAleatorio == prmNumero);
+        return ganoSorteo;
     }
 }
